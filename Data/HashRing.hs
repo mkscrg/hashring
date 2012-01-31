60a6c82526be932bddcd56dea32586ee346f73de
Michael S. Craig <mkscrg@gmail.com>
2012-01-31 18:27:27 -0500

    Various improvements, moving towards hackage push

--- Data/HashRing.hs
+++ Data/HashRing.hs
@@ -1,13 +1,77 @@
-module Data.HashRing
-( HashRing
+-- | module: Data.HashRing
+-- copyright: (c) Michael S. Craig 2012
+-- license: BSD3
+-- maintainer: mkscrg@gmail.com
+-- stability: experimental
+-- portability: portable
+--
+-- An efficient implementation of consistent hashing, as described in
+--
+--    * David Karger et al., \"/Consistent hashing and random trees:
+--    distributed caching protocols for relieving hot spots on the World Wide
+--    Web/\", 29th Annual ACM Symposium on Theory,
+--    <http://dl.acm.org/citation.cfm?id=258660>
+--
+-- In distributed computing applications, it's usually necessary route messages
+-- to some group of N nodes in the network. Message locality, wherein messages
+-- of the same kind are routed to the same node, is often desirable. \"Normal\"
+-- hashing, where a message's node is determined by some hash function modulo
+-- N, has the undesirable property that adding or removing a node from the
+-- network causes the key sets of all other nodes to change drastically.
+-- In contrast, consistent hashing has the property that small changes to the
+-- size of the node set cause only small changes to key sets of the nodes.
+--
+-- This implementation is built on top of 'IntMap' and 'Set'. It provides
+-- /O(1)/ lookup functions as well as /O(min(log n, R))/ insertion and deletion
+-- functions, where /R/ is the number of replica nodes used in the ring (see
+-- 'empty').
+--
+-- The key space of the ring is the full range of 'Int' values. To insert a
+-- node, we generate (/R > 0/) keys by hashing the node with /R/ successive
+-- salts, and the node is referenced by those keys in the ring. To get a node
+-- for a message, we hash the message to an 'Int' value /k/ and find the
+-- smallest key /k'/ in the ring such that /k <= k'/. The node is the value
+-- referenced by /k'/. Higher values of /R/ give a more even distribution of
+-- keys to nodes but slow down insertions and deletions of nodes. /R/ is
+-- specified when constructing a 'HashRing' with 'empty', 'singleton', or
+-- 'fromList' and retrievable with 'nReplicas'.
+--
+-- The ability of 'HashRing' to fairly distribute messages among nodes relies
+-- on the implementations of 'Data.Hashable.hashWithSalt' for the message and
+-- node types. For example, the default implementation for
+-- 'Data.ByteString.ByteString' is non-uniform on short inputs, and so it's
+-- unsuitable for use with 'HashRing'. Reimplementing
+-- 'Data.Hashable.hashWithSalt' for your message and node types with a
+-- cryptographic hash function (like MD5 or SHA1 from the @cryptohash@ package)
+-- will give better results.
+
+module Data.HashRing (
+  -- * Map type
+  HashRing
+, def
+, nReplicas
+  -- * Construction
 , empty
-, fromList
-, insert
-, delete
+, singleton
+  -- * Operators
+, (!)
+  -- * Query
+, null
+, size
+, member
 , lookup
+, find
+  -- * Insertion
+, insert
+  -- * Deletion
+, delete
+  -- * Conversion
+, fromList
+, toList
 ) where
 
 import Prelude hiding (lookup)
+import Data.Default (Default (def))
 import Data.Hashable (Hashable, hash, hashWithSalt)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
@@ -15,40 +79,84 @@ import Data.Set (Set)
 import qualified Data.Set as S
 
 
+-- | The constructor for this data type is not exported. See 'empty',
+-- 'singleton', or 'fromList'. Its 'def' value is an empty ring with /R = 3/.
+--
+-- Note that 'HashRing' is parameterized by the node type and not by the
+-- message type. As made clear by the type signatures for '!', 'find', and
+-- 'lookup', any 'Hashable' type can be used as a message.
 data HashRing a = HashRing
-                { nodeMap :: IntMap a
-                , nodeSet :: Set a
-                , nReplicas :: Int }
+  { nodeMap :: IntMap a
+  , nodeSet :: Set a
+  , nReplicas :: Int
+  -- ^ Number of replica nodes (/R/) in the ring for each real node.
+  }
 
+
+--instance Show a => Show (HashRing a) where
+
+--instance Read a => Read (HashRing a) where
+
+instance Default (HashRing a) where
+  def = HashRing IM.empty S.empty 3
+
+
+-- | Construct an empty ring with a specific /R/ value.
 empty :: Int -> HashRing a
 empty nreps = HashRing IM.empty S.empty nreps
 
-fromList :: (Ord a, Hashable a) => [a] -> Int -> HashRing a
-fromList nodes nreps = foldr insert (empty nreps) nodes
+-- | Construct a single-node ring with a specific /R/ value.
+singleton :: (Ord a, Hashable a) => Int -> a -> HashRing a
+singleton nreps node = insert node $ empty nreps
+
+
+null :: HashRing a -> Int
+null ring = S.null $ nodeSet ring
+
+-- | Get the node in the ring corresponding to a message, or @Nothing@ if the
+-- ring is empty
+lookup :: Hashable b => b -> HashRing a -> Maybe a
+lookup msg ring = let nmap = nodeMap ring in if IM.null nmap
+  then Nothing
+  else Just $ case IM.splitLookup (hash msg) nmap of
+    (_, Just node, _) -> node
+    (_, _, submap) -> if IM.null submap
+      then snd $ IM.findMin nmap
+      else snd $ IM.findMin submap
+
+find :: Hashable b => b -> HashRing a -> a
+find msg ring = case lookup msg ring of
+  Just x -> x
+  Nothing -> error "HashRing.find: empty hash ring"
+
+(!) :: Hashable b => b -> HashRing a -> a
+msg ! ring = find msg ring
+
 
 insert :: (Ord a, Hashable a) => a -> HashRing a -> HashRing a
 insert node ring = ring
-    { nodeMap = foldr (\key nmap -> IM.insert key node nmap) (nodeMap ring)
-              $ take (nReplicas ring)
-              $ filter (\key -> not $ IM.member key (nodeMap ring))
-              $ nodeKeys node
-    , nodeSet = S.insert node (nodeSet ring) }
+  { nodeMap = foldr (\key nmap -> IM.insert key node nmap) (nodeMap ring)
+            $ take (nReplicas ring)
+            $ filter (\key -> not $ IM.member key (nodeMap ring))
+            $ nodeKeys node
+  , nodeSet = S.insert node (nodeSet ring) }
 
 delete :: (Ord a, Hashable a) => a -> HashRing a -> HashRing a
 delete node ring = if not $ S.member node (nodeSet ring) then ring else ring
-    { nodeMap = foldr (\key nmap -> IM.delete key nmap) (nodeMap ring)
-              $ take (nReplicas ring)
-              $ filter (\key -> IM.lookup key (nodeMap ring) == Just node)
-              $ nodeKeys node
-    , nodeSet = S.delete node (nodeSet ring) }
-
-lookup :: Hashable b => b -> HashRing a -> Maybe a
-lookup msg ring = if IM.null (nodeMap ring)
-    then Nothing
-    else Just $ case IM.splitLookup (hash msg) (nodeMap ring) of
-        (_, Just node, _) -> node
-        (_, _, submap) -> snd $ IM.findMin submap
+  { nodeMap = foldr (\key nmap -> IM.delete key nmap) (nodeMap ring)
+            $ take (nReplicas ring)
+            $ filter (\key -> IM.lookup key (nodeMap ring) == Just node)
+            $ nodeKeys node
+  , nodeSet = S.delete node (nodeSet ring) }
 
 
+fromList :: (Ord a, Hashable a) => Int -> [a] -> HashRing a
+fromList nreps nodes = foldr insert (empty nreps) nodes
+
+toList :: HashRing a -> [a]
+toList ring = S.toList $ nodeSet ring
+
+
+-- List of possible keys for a new node
 nodeKeys :: Hashable a => a -> [Int]
 nodeKeys node = map (\i -> hashWithSalt i node) [0..]
